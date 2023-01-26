577d97d8921dcb6d5c7e54b4704eadd31809acaa
Chris Ryan <chrisryanassociates@gmail.com>
2023-01-26 09:26:31 -0700

    public error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.use(express.static(`${__dirname}../`));
+app.use(express.static(`${__dirname}../public`));
 
 app.listen(4555, () => {
     console.log('Server running on port 4555')
